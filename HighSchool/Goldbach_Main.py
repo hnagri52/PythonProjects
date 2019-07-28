cc846911a559ea09411da5fd605ddd7e3e9748b1
Hussein Nagri <husseinnagri@v1041-wn-rt-d-237-254.campus-dynamic.uwaterloo.ca>
2019-07-28 10:41:08 -0400

    update organization

--- HighSchool/Goldbach_Main.py
+++ HighSchool/Goldbach_Main.py
@@ -1,10 +1,10 @@
-import Goldbach_Conjecture
+from HighSchool import Goldbach_Conjecture
 
 #Repeatedly get numbers and test each one to see
 #if Goldbach's Conjecture is correct for that value.
 #The value zero causes the program to stop
 
-value=Goldbach_Conjecture.nextNumber()
+value= Goldbach_Conjecture.nextNumber()
 while value!=0:
     Goldbach_Conjecture.testGoldbach(value)
-    value=Goldbach_Conjecture.nextNumber()
+    value= Goldbach_Conjecture.nextNumber()
