3ebb0d9ca0755be4855532a82716acf60ba31f15
Hussein Nagri <husseinnagri@v1041-wn-rt-d-237-254.campus-dynamic.uwaterloo.ca>
2019-07-29 11:47:49 -0400

    join function and strip and replace

---
+++ LearningScripts/joinfunctions.py
@@ -0,0 +1,17 @@
+
+
+ls = ["Hussein", "sabi", "jy", "ahmed"]
+print(", ".join(ls))
+
+var = "My name is Hussein"
+var2 = "    MY na   mmee kr    er    gff     "
+
+var2 = var2.strip()
+print(var2)
+var2 = var2.replace(" ", "")
+print(var2)
+
+
+a = var.replace(" ", "")
+print(a)
+print( "letter: " + " letter: ".join(a) )
