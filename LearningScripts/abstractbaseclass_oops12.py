9ce64dfa15da96dd3d380e36a9f3236bb2521474
Hussein Nagri <hznagri@uwaterloo.ca>
2019-08-14 23:52:29 -0400

    property decorators

--- LearningScripts/abstractbaseclass_oops12.py
+++ LearningScripts/abstractbaseclass_oops12.py
@@ -1,8 +1,29 @@
 #abstract method basically tells all of the inheriting classes that they need a certain method defined
 
-from abc import abstractmethod, ABCMeta
+from abc import abstractmethod, ABCMeta, ABC
 
 class Shape(metaclass=ABCMeta):
-    
+#class Shape (ABC):  ---> this will do the exact same
+    @abstractmethod
+    def printArea(self):
+        return 0
+
+
+class Rectangle(Shape):
+    type = "rectangle"
+    sides = 4
+
+    def __init__(self):
+        self.length = 6
+        self.width = 4
+
+    #def printArea(self):
+    #if you dont have it, it will throw error
+
+#the reason we do this is to have our inheriting class to include certain things and we dont forget
+#we cannot create objects of the abstract base class FYI
+
+rect1 = Rectangle()
+
 
 
---
+++ LearningScripts/settersnpropertydecorators_oops13.py
@@ -0,0 +1,45 @@
+class Employee:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+        # self.email = f"{fname}.{lname}@hussy.com"
+
+    def explain(self):
+        return f"This employee is {self.fname} {self.lname}"
+
+#use a decorator change the function email into a property instead.. so dont have to call as function
+    @property
+    def email(self):
+        if self.fname==None or self.lname == None:
+            return "Email is not set. Please set it using setter"
+        return f"{self.fname}.{self.lname}@hussy.com"
+#must use decorator to still set the email
+    @email.setter
+    def email(self, string):
+        print("Setting now...")
+        names = string.split("@")[0]
+        self.fname = names.split(".")[0]
+        self.lname = names.split(".")[1]
+
+#we also need to create a custom decorator to delete the property
+    
+    @email.deleter
+    def email(self):
+        self.fname = None
+        self.lname = None
+
+
+Huss= Employee("Hussein", "Nagri")
+
+print(Huss.email)
+
+Huss.fname = "US"
+
+print(Huss.email)
+Huss.email = "this.that@hussy.com"
+print(Huss.fname)
+
+del Huss.email
+print(Huss.email)
+Huss.email = "Harry.Perry@hussy.com"
+print(Huss.email)
