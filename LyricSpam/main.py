f0835f1b8c9721612f9b5d9b85c01dab3b546335
Hussein <hznagri@uwaterloo.ca>
2020-05-24 22:46:46 -0400

    send youtube link aswell

--- LyricSpam/main.py
+++ LyricSpam/main.py
@@ -8,6 +8,7 @@ from twilio.rest import Client
 
 load_dotenv()
 
+
 def main():
     YOUTUBE_SECRET_KEY = os.getenv("YOUTUBE_API")
     HAPPI_SECRET_KEY = os.getenv("HAPPI_API")
@@ -17,15 +18,15 @@ def main():
 
     text = input("Enter a song name, and their artist name: ")
 
+
     while(text != 'q'):
-        # delete youtube api get youtube id cuz other api not working as well
-        # payload = {'part': 'snippet', 'key': YOUTUBE_SECRET_KEY, 'order': 'relevance', 'q': text, 'maxResults': 1}
-        # resp = requests.Session().get('https://www.googleapis.com/youtube/v3/search', params=payload)
-        # resp_dict = json.loads(resp.content)
-        # youtube_id = resp_dict['items'][0]["id"]["videoId"]
+        payload = {'part': 'snippet', 'key': YOUTUBE_SECRET_KEY, 'order': 'relevance', 'q': text, 'maxResults': 1}
+        resp = requests.Session().get('https://www.googleapis.com/youtube/v3/search', params=payload)
+        resp_dict = json.loads(resp.content)
+        youtube_id = resp_dict['items'][0]["id"]["videoId"]
 
         # apiurl = requests.get(f"https://www.ytvdgrabber.com/api/get?youtubeid={youtube_id}")
-        #
+        # ^^ was for bollywood stuff
         # print(apiurl.content)
 
         apiUrl = requests.get(f"https://api.happi.dev/v1/music?q={text}&limit=1&apikey={HAPPI_SECRET_KEY}&type=track")\
@@ -35,7 +36,6 @@ def main():
         if (len(apiUrl["result"]) == 0 or apiUrl["result"][0]["haslyrics"] == False ):
             print(apiUrl, "\n")
             text = input("No lyrics found with those search results! Enter the song name you want to play!")
-
             continue
 
         lyricLink = apiUrl["result"][0]["api_lyrics"]
@@ -52,18 +52,28 @@ def main():
         listOfWords = lyrics.split("\n")
         listOfWords = [x for x in listOfWords if x != '']
 
+
+        #send youtube link to video
+        sendMessage(f"Here is a link to the song: https://www.youtube.com/watch?v={youtube_id}", client)
+
+        #send lyrics
         for word in listOfWords:
-            message = client.messages \
-                .create(
-                body=f'{word}',
-                from_='+18706863507',
-                to='+16478915152'
-            )
-            time.sleep(3)
+            sendMessage(word, client)
+            time.sleep(5)
 
 
 
         text = input("Enter the song name you want to play!")
+
+
+def sendMessage(word, client):
+    message = client.messages \
+        .create(
+        body=f'{word}',
+        from_='+18706863507',
+        to='+16473088537'
+    )
+    print(f"Just sent: {word}")
 
 
 if __name__ == '__main__':
