caf8172b1983c62e6452c8f4a60d476a204b73a3
Hussein <hznagri@uwaterloo.ca>
2020-06-03 23:43:31 -0400

    add start date

--- iCal/calEvent.py
+++ iCal/calEvent.py
@@ -1,3 +1,21 @@
+import datetime
+
+
 class CalEvent:
     def __init__(self, desc=""):
         self.desc = desc
+
+    def validate_and_add(self, date_text, date_type):
+        try:
+             datetime_obj = datetime.date.fromisoformat(date_text)
+             if(isinstance(datetime_obj, datetime.date)):
+                self.add_date(datetime_obj, date_type)
+        except:
+            raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+
+    def add_date(self, date, type):
+        if(type=="start"):
+            self.start_date = date
+        else:
+            self.end_date = date
+
--- iCal/main.py
+++ iCal/main.py
@@ -1,11 +1,16 @@
 from calEvent import CalEvent
+import datetime
+
 
 def main():
+    #obtain user input for desc and start date
     desc = input("Hey! Please enter a description of the event that you want to schedule!")
     theNewEvenet  = CalEvent(desc)
 
-    print(theNewEvenet.desc)
+    # validate and add the start date
+    start_date = input("Please enter the start date (YYYY-MM-DD): ")
+    theNewEvenet.validate_and_add(start_date, "start")
 
-
+    print(theNewEvenet.start_date, theNewEvenet.desc)
 if __name__ == '__main__':
     main()
