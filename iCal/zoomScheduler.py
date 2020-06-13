7693c8563ed0b0334399da46aab28a79e2e08c1f
Hussein <hznagri@uwaterloo.ca>
2020-06-13 16:25:11 -0400

    zoom API integrated

--- iCal/zoomScheduler.py
+++ iCal/zoomScheduler.py
@@ -1,16 +1,23 @@
 from dotenv import load_dotenv
 import os
+import json
 import requests
 load_dotenv()
 
 
 class ZoomScheduler:
+    #TODO: JWT generation should come dynamically, not have a default in the .env file
     def __init__(self):
         self.zoom_key = os.getenv("ZOOM_API_KEY")
         self.email = os.getenv("ZOOM_EMAIL")
+        self.JWT = os.getenv("ZOOM_JWT")
     def send_invite(self, creation_details):
-        URL = f"https://api.zoom.us/v2/users/{self.email}/meetings"
-        headers = {"Content-Type":"application/json", "Authorization": f"Bearer ${self.zoom_key}"}
-        res = requests.post(URL, headers=headers)
-        print(res)
-        print(creation_details)
+        URL = f"https://api.zoom.us/v2/users/{self.email}/meetings?access_token={self.JWT}"
+        print(URL)
+        data = json.dumps({})
+        headers = {
+            "Content-Type":"application/json"
+        }
+        res = requests.post(URL, data=data, headers=headers)
+        print(res.content)
+
