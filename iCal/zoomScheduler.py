895d554709a585cc60da6c8ae231fa8c18249a7c
Hussein <hznagri@uwaterloo.ca>
2020-06-15 20:53:30 -0400

    receive emails

--- iCal/zoomScheduler.py
+++ iCal/zoomScheduler.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import json
 import requests
+import re
 load_dotenv()
 
 
@@ -13,15 +14,30 @@ class ZoomScheduler:
         self.JWT = os.getenv("ZOOM_JWT")
     def send_invite(self, creation_details):
         URL = f"https://api.zoom.us/v2/users/{self.email}/meetings?access_token={self.JWT}"
+
+        emails = self.get_details()
+
+
+
+
+
         data = json.dumps({})
         headers = {
             "Content-Type":"application/json"
         }
         res = requests.post(URL, data=data, headers=headers)
-        print(res.content)
 
 
     def get_details(self):
         to_send = input("Enter the email of the zoom participants, separated by a comma").split(",")
+        for email in to_send:
+            self.check(email)
+        return to_send
 
-
+    def check(self, email):
+        #regex for email
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        if (re.search(regex, email)):
+            pass
+        else:
+            raise ValueError(f'Invalid email provided: {email}')
