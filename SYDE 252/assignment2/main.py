3900be7bbf224d1c495c384701f2a8b53dcf01c8
Hussein <hznagri@uwaterloo.ca>
2020-09-27 19:06:45 -0400

    fix axis

--- SYDE 252/assignment2/main.py
+++ SYDE 252/assignment2/main.py
@@ -3,20 +3,13 @@ import numpy as np
 
 
 def plot_graph():
-    t = np.linspace(-10, 10, 1000)
-    # plt.subplot(2, 1, 1);
-    # plt.plot(t, np.exp(-2j * np.pi * t).real);
-    # plt.xlabel('t');
-    # plt.ylabel('Re x(t)');
-    # plt.title(r'Real part of  $x(t)=e^{-2j \pi t}$');
-    # plt.xlim([-10, 10]);
+    t = np.arange(-3, 3, 0.01)
 
-    # plt.subplot(2, 1, 2);
-    plt.plot(t, -1 * np.exp(-2 * t) * (1j * np.sin(2 * np.pi * t)).imag);
-    plt.xlabel('t');
-    plt.ylabel('Im x(t)');
-    plt.title(r'Imaginary part of  $x(t)=e^{-2j \pi t}$');
-    plt.xlim([-11,10]);
+    plt.plot(t, np.exp(-2 * (1 + 1j * np.pi) * t).imag)
+    plt.xlabel('t')
+    plt.ylabel('Im x(t)')
+    plt.title(r'Imaginary part of  $x(t)=e^{-2j \pi t}$')
+    plt.xlim([-10,10])
     plt.show()
 
 
