3457ceb063b8157c4bc69e80ee53c1c1a1797bc1
Hussein <hznagri@uwaterloo.ca>
2020-10-12 14:54:51 -0400

    no profit

---
+++ School/262/a3/graph_2.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_graph():
+    t = np.arange(0, 12)
+
+    plt.plot(t, t-t+7, label="Marginal Revenue", color="red")
+    plt.plot(t, (1/4)* pow((t-3),2) + 3, label="Marginal Cost", color="blue")
+    plt.plot(t, (1/8)* pow((t-3),2) + 5, label="Average Total Cost", color="green")
+    plt.xlabel('Quantity')
+    plt.ylabel('Price')
+    plt.title(r'Competitive market: No Economic Profit')
+    plt.xlim([0,12])
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_graph()
